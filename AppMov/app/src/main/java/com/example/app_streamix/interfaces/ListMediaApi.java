28bf293fa4a09049eadde737a578d9435feaeb7d
Bea <justaletterwriter.al@gmail.com>
2025-02-08 10:15:52 +0000

    alterações minimas

--- AppMov/app/src/main/java/com/example/app_streamix/interfaces/ListMediaApi.java
+++ AppMov/app/src/main/java/com/example/app_streamix/interfaces/ListMediaApi.java
@@ -7,7 +7,7 @@ import com.example.app_streamix.models.ListMedia;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
-import retrofit2.http.GET;import retrofit2.http.GET;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
